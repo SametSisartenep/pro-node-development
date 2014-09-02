790db9f4e3b3af6ff7218c688f8b42ccf2fa7c50
SametSisartenep <krashbuffer@gmail.com>
2014-09-02 19:55:28 +0200

    Added server.

---
+++ server.js
@@ -0,0 +1,8 @@
+var http = require('http');
+
+http.createServer(function ( request, response ) {
+	response.writeHead(200, {"Content-Type": "text/html"});
+	response.end("<h1>Welcome to Pro Node.js Development</h1>");
+}).listen(1337);
+
+console.log("Server started at \"http://127.0.0.1:1337\"");
