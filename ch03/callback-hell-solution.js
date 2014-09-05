380e708c9d17071370a0c43fac516ddbcb902f66
SametSisartenep <krashbuffer@gmail.com>
2014-09-05 15:24:31 +0200

    Callback Hell solution written.

---
+++ ch03/callback-hell-solution.js
@@ -0,0 +1,32 @@
+var fs = require('fs');
+var fileName = "foo.txt";
+
+function cbReadFile ( error, data ) {
+	if (error)
+	{
+		throw error;
+	}
+
+	console.log(data);
+}
+
+function cbStat ( error, stats ) {
+	if (error)
+	{
+		throw error;
+	}
+
+	if (stats.isFile())
+	{
+		fs.readFile(fileName, "utf8", cbReadFile);
+	}
+}
+
+function cbExists ( exists ) {
+	if (exists)
+	{
+		fs.stat(fileName, cbStat);
+	}
+}
+
+fs.exists(fileName, cbExists);
